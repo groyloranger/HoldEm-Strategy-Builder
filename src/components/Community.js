92bd865929c8dc877c1d08476c154584eabfea35
groyloranger <dev+github@decisiveroll.com>
2018-04-13 09:41:41 -0400

    Render board as a string
    The component receives the board as an array of card values and renders
    it as a single string.
    
    The input is defined as an array since it is assumed it will be
    manipuled that way higher in the component hierarchy.

--- src/components/Community.js
+++ src/components/Community.js
@@ -3,14 +3,28 @@ import React from 'react';
 
 // import '../styles/Community.css';
 
-// type Props = {}
+type Props = {
+  board: string[], // external optional
+}
 
-export default function Community(/* props: Props */) {
+export default function Community(props: Props) {
+  const boardToString = () => {
+    const cards = props.board.slice();
+    let str = '';
+    for (let len = 5; len > 0; len -= 1) {
+      const card = ((cards.length === len) ? cards.pop().concat(' ') : '_ ');
+      str = card.concat(str);
+    }
+    return str.trim();
+  };
+
   return (
     <div>
-      Community
+      {boardToString()}
     </div>
   );
 }
 
-// Community.defaultProps = {};
+Community.defaultProps = {
+  board: [],
+};
--- src/stories/Community.story.js
+++ src/stories/Community.story.js
@@ -5,6 +5,15 @@ import { storiesOf } from '@storybook/react';
 import Community from '../components/Community';
 
 storiesOf('Community', module)
-  .add('default', () => (
+  .add('preflop', () => (
     <Community />
+  ))
+  .add('flop', () => (
+    <Community board={['As', '7h', '2d']} />
+  ))
+  .add('turn', () => (
+    <Community board={['As', '7h', '2d', '3c']} />
+  ))
+  .add('river', () => (
+    <Community board={['As', '7h', '2d', '3c', 'Jd']} />
   ));
